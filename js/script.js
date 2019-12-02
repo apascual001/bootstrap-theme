843f581d7bb62d7fce66d62a6bfaefcd80510df6
Ana Pascual <annapascual001@gmail.com>
2019-12-02 11:46:12 +0100

    Corregit prevent default

--- js/script.js
+++ js/script.js
@@ -9,58 +9,54 @@ var name_error = document.getElementById('name_error');
 var email_error = document.getElementById('email_error');
 var state_error = document.getElementById('state_error');
 var password_error = document.getElementById('password_error');
+//var password_confirm_error = document.getElementById('pass_confirm_div');
+
 // SETTING ALL EVENT LISTENERS
 //username.addEventListener('input',validate,true);
 //email.addEventListener('blur', validate, true);
 //state.addEventListener('blur', validate, true);
 //password.addEventListener('blur', validate, true);
 
+document.getElementById("#enviar").addEventListener("click", function(event) {
+         event.preventDefault();
+}, false);
 
-/*window.onload = iniciar; 
-function iniciar() {
-    document.getElementById("enviar").addEventListener('click', validate, false);
-}
-*/
 
-function comprobar(e) { 
+function comprobar() { 
   var x = 0; 
   console.log(x);
   if (username.value.length < 3) {
     username.style.border = "1px solid red";
     document.getElementById('username_div').style.color = "red";
     name_error.textContent = "El Nombre de usuario debe tener 3 letras mínimo";
-    username.focus();
+    //username.focus();
     x ++;
     console.log(x);
-    e.preventDefault();
   }
   if (email.value == "") {
     email.style.border = "1px solid red";
     document.getElementById('email_div').style.color = "red";
     email_error.textContent = "Email es requerido";
-    email.focus();
+    //email.focus();
     x ++;
     console.log(x);
-    e.preventDefault();
   }
 
   if (state.value == "Escoja Provincia") {
     state.style.border = "1px solid red";
     document.getElementById('state_div').style.color = "red";
     state_error.textContent = "Provincia es requerido";
-    state.focus();
+    //state.focus();
     x ++;
     console.log(x);
-    e.preventDefault();
   }
   if (password.value == "") {
     password.style.border = "1px solid red";
     document.getElementById('password_div').style.color = "red";
     password_error.textContent = "Password es requerido";
-    password.focus();
+    //password.focus();
     x ++;
     console.log(x);
-    e.preventDefault();
   }
   if (password.value != password_confirm.value) {
     password_confirm.style.border = "1px solid red";
@@ -68,19 +64,17 @@ function comprobar(e) {
     password_confirm_error.innerHTML = "Las dos contraseñas no coinciden";
     x ++;
     console.log(x);
-    e.preventDefault();
   }
   return x;
 }
-function validate(e) {
+function validate() {
   limpiar();
   var x = comprobar();
   if (x == 0) {
     console.log(x);
     return true;
     } else {
-        e.preventDefault();
-        return false;
+    return false;
     }
   }
 // validation function
@@ -116,14 +110,14 @@ function limpiar(){
 }
   if (password.value != "") {
     password.style.border = "1px solid #5e6e66";
-    document.getElementById('pass_confirm_div').style.color = "#5e6e66";
+    //document.getElementById('pass_confirm_div').style.color = "#5e6e66";
     document.getElementById('password_div').style.color = "#5e6e66";
     password_error.innerHTML = "";
   }
   if (password.value === password_confirm.value) {
-    password.style.border = "1px solid #5e6e66";
+    password_confirm.style.border = "1px solid #5e6e66";
     document.getElementById('pass_confirm_div').style.color = "#5e6e66";
-    password_error.innerHTML = "";
+    password_confirm_error.innerHTML = "";
   }
 
 }
